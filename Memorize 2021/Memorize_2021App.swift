ef97d213f19eca7db191f5759bbe4003f4a86cc1
Stas <staszhingel@icloud.com>
2021-10-06 13:16:44 +0300

    Initial Commit

---
+++ Memorize 2021/Memorize_2021App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_2021App.swift
+//  Memorize 2021
+//
+//  Created by Стас Жингель on 06.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
