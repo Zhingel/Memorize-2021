10543fe62bfb06e27daa10cc6b93b328e64a6a6e
Stas <staszhingel@icloud.com>
2021-10-07 16:55:29 +0300

    lesson 3

--- Memorize 2021/ContentView.swift
+++ Memorize 2021/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["✈️","🚀","🛸","🚂","🚐","🚒","🛻","🛴","🚲","🛵","🏍","🚘","🚠","🚄","🚆","🛶","⛵️","🚁"]
+    @State var emojisCount = 12
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojisCount], id:\.self) { emoji in
+                        CardView(content: emoji)
+                                    .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            Spacer()
+            HStack {
+                remove
+                Text("Shuffle")
+                add
+            }
+            .font(.title)
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+    var remove: some View {
+        Button(action: {
+            if emojisCount > 1 {
+                emojisCount -= 1
+            }
+        }) {
+            Image(systemName: "minus.circle.fill")
+        }
+    }
+    var add: some View {
+        Button( action: {
+            if emojisCount < emojis.count {
+                emojisCount += 1
+            }
+        }) {
+            Image(systemName: "plus.circle.fill")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
---
+++ Memorize 2021/MemoryGame.swift
@@ -0,0 +1,20 @@
+//
+//  MemoryGame.swift
+//  Memorize 2021
+//
+//  Created by Стас Жингель on 07.10.2021.
+//
+
+import Foundation
+
+struct MemoryGame {
+    var cards: [Card]
+    
+    func choose(_ card: Card) {
+        
+    }
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+    }
+}
